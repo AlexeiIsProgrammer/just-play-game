e1d78ad9405732f64fe3aa90aa8ec3e3e68b98cc
Alexei <avenge.now.r@mail.ru>
2024-02-25 20:40:08 +0300

    fix: change mode to the dark

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,19 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App.tsx';
 import './index.css';
 import theme from './theme.ts';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import { store } from './redux/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </Provider>
-  //</React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </Provider>
+  </React.StrictMode>
 );
