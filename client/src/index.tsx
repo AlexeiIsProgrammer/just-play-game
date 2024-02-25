07b6fccaf9ebb7253cf3f3077cba09d356cb06f9
Alexei <avenge.now.r@mail.ru>
2024-02-25 20:28:58 +0300

    try to set dark mode as default in chakra ui

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,17 +2,16 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App.tsx';
 import './index.css';
 import theme from './theme.ts';
-import { ChakraBaseProvider, ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import { store } from './redux/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <ChakraBaseProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>
       <App />
-    </ChakraBaseProvider>
+    </ChakraProvider>
   </Provider>
   //</React.StrictMode>
 );
